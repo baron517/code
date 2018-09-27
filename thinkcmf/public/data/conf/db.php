517e36a0f3f6f54c170e782a0422690e1d2ab236
baron517 <450175465@qq.com>
2018-09-27 21:43:12 +0800

    a

---
+++ thinkcmf/public/data/conf/db.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * 配置文件
+ */
+return array(
+    'DB_TYPE' => 'mysql',
+    'DB_HOST' => 'localhost',
+    'DB_NAME' => 'xiuqiu',
+    'DB_USER' => 'root',
+    'DB_PWD' => 'tuyao19910715',
+    'DB_PORT' => '3306',
+    'DB_PREFIX' => 'tb_',
+    //密钥
+    "AUTHCODE" => 'yW5ysjO3joU5OLeFX9',
+    //cookies
+    "COOKIE_PREFIX" => '9o4COW_',
+);
